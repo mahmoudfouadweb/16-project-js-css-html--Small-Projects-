06615cccb4c352cfe8b8187649d08d1f92728694
mahmoud fouad <mfapolice@gmail.com>
2022-07-09 03:42:57 +0200

    finsh Project

--- -- 1. digital-clock/script.js
+++ -- 1. digital-clock/script.js
@@ -1,7 +1,28 @@
 'use strict';
-const h = document.querySelector('.h'),
+let h = document.querySelector('.h'),
   min = document.querySelector('.min'),
   s = document.querySelector('.s'),
   ampm = document.querySelector('.ampm');
 
-document.addEventListener('DOMContentLoaded', function () {});
+function updateTime() {
+  let hours = new Date().getHours(),
+    minute = new Date().getMinutes(),
+    second = new Date().getSeconds();
+  h.textContent = hours;
+  min.textContent = minute;
+  s.textContent = second;
+  let amPm = 'AM';
+  if (hours > 12) {
+    hours -= 12;
+    amPm = 'PM';
+  } else {
+    amPm = 'AM';
+  }
+  ampm.textContent = amPm;
+}
+// console.log(hours, minute, second);
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(() => {
+    updateTime();
+  }, 1000);
+});
