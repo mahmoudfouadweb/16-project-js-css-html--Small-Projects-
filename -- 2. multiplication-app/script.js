53d9a9edc419a9d8d944cc6b7c78478400987bd6
mahmoud fouad <mfapolice@gmail.com>
2022-07-09 23:19:35 +0200

    update

--- -- 2. multiplication-app/script.js
+++ -- 2. multiplication-app/script.js
@@ -6,3 +6,19 @@ const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 const answer = document.querySelector('.answer');
 const btn = document.querySelector('.btn');
+
+// variables validation
+// console.log(score, one, two, answer, btn);
+
+// random numbers function
+
+function randomNumber() {
+  return Math.floor(Math.random() * 9 + 1);
+}
+
+// multiply function
+function mulitply() {
+  one.textContent = randomNumber();
+  two.textContent = randomNumber();
+}
+mulitply();
