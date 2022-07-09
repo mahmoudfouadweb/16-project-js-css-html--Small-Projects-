298fb4d59b509f8805b7b5e2aeca938d0b0fb150
mahmoud fouad <mfapolice@gmail.com>
2022-07-09 23:12:26 +0200

    init javascript

--- -- 2. multiplication-app/script.js
+++ -- 2. multiplication-app/script.js
@@ -1,2 +1,8 @@
 'use strict';
-console.log('hi there');
+
+// GENERAL VARIABLES
+const score = document.querySelector('.score-value');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const answer = document.querySelector('.answer');
+const btn = document.querySelector('.btn');
