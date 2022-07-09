7c75904cefd989933a32ba2129e0d8294f7743d3
mahmoud fouad <mfapolice@gmail.com>
2022-07-10 01:13:59 +0200

    finish

--- -- 2. multiplication-app/script.js
+++ -- 2. multiplication-app/script.js
@@ -10,36 +10,53 @@ const btn = document.querySelector('.btn');
 // variables validation
 // console.log(score, one, two, answer, btn);
 
-// random numbers function
+// helper variable
+let userScore = JSON.parse(localStorage.getItem('score'));
+if (!userScore) {
+  userScore = 0;
+}
+const num2 = randomNumber();
+const num1 = randomNumber();
 
+// multiplication
+const result = num1 * num2;
+console.log('result', result);
+
+// random numbers function
 function randomNumber() {
   return Math.floor(Math.random() * 9 + 1);
 }
 
-// multiply function
-function mulitply() {
-  const first = randomNumber();
-  const second = randomNumber();
-  console.log(answer);
-  one.textContent = first;
-  two.textContent = second;
-  return first * second;
+// updateUI function
+function updateUI() {
+  one.textContent = num1;
+  two.textContent = num2;
+  score.textContent = userScore;
 }
-let result = mulitply();
-console.log('result', result);
-
-btn.addEventListener('click', function (e) {
-  // e.preventDefault();
-  let userInput = Number(answer.value);
-  let userScore = 0;
-  if (result == userInput) {
-    console.log('true');
-    userScore++;
-    score.textContent = userScore;
-    mulitply();
-    console.log(result);
-  } else {
-    console.log('false');
-    userScore--;
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  updateUI();
+  btn.addEventListener('click', function (e) {
+    // e.preventDefault();
+    let userInput = +answer.value;
+    if (result == userInput) {
+      console.log('true');
+      userScore++;
+      updateLocalstorage();
+      score.textContent = userScore;
+      console.log('result', result);
+      updateUI();
+    } else {
+      console.log('false');
+      userScore--;
+      updateLocalstorage();
+      score.textContent = userScore;
+      console.log('result', result);
+      updateUI();
+    }
+  });
 });
+
+// local storage
+const updateLocalstorage = function () {
+  localStorage.setItem('score', JSON.stringify(userScore));
+};
