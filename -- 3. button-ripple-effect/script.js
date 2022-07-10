4aab9c813422f4fb28cc819b9396236b09e729b5
mahmoud fouad <mfapolice@gmail.com>
2022-07-11 00:02:16 +0200

    done javascript

--- -- 3. button-ripple-effect/script.js
+++ -- 3. button-ripple-effect/script.js
@@ -1,2 +1,9 @@
 'use strict';
-console.log('hi there');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover', e => {
+  const x = e.pageX - btn.offsetLeft;
+  const y = e.pageY - btn.offsetTop;
+  btn.style.setProperty('--xPos', x + 'px');
+  btn.style.setProperty('--yPos', y + 'px');
+});
