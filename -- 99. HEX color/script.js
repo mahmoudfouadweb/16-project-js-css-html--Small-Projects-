2c4378ec3e13574cd4ba8afab544fe4904c31aed
mahmoud fouad <mfapolice@gmail.com>
2022-07-10 22:57:15 +0200

    updates

--- -- 99. HEX color/script.js
+++ -- 99. HEX color/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
-const arr = ['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6'];
+// GENERAL VARIABLES
+const code = document.querySelector('.code'),
+  copied = document.querySelector('.copied'),
+  btn = document.querySelector('.btn'),
+  arr = 'abcdef0123456789';
+
+// click event
+btn.addEventListener('click', () => {
+  let hash = '#';
+  for (let i = 0; i < 6; i++) {
+    hash += arr[Math.floor(Math.random() * arr.length)];
+  }
+  code.textContent = hash;
+  document.body.style.backgroundColor = hash;
+  document.body.style.transition = 'all 0.5s';
+  return hash;
+});
+code.addEventListener('click', e => {
+  navigator.clipboard.writeText(code.textContent);
+  copied.style.opacity = 1;
+});
