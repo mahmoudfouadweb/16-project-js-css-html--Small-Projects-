cccf90785498bc5f31ae47cc7c7476aee3d47569
mahmoud fouad <mfapolice@gmail.com>
2022-07-14 06:36:09 +0200

    done

--- -- 6. sticky-navbar/script.js
+++ -- 6. sticky-navbar/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// DOM variables
+const nav = document.querySelector('.nav');
+const first = document.querySelector('#first');
+
+window.addEventListener('scroll', () => {
+  const locat = first.offsetTop - window.scrollY - 38.4 - nav.offsetHeight;
+  console.log(locat < 0);
+  if (locat <= 0) {
+    nav.classList.add('active');
+  } else {
+    nav.classList.remove('active');
+  }
+});
+
+// console.log(nav.offsetHeight);
