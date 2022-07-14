373beb8ee794730d9da40f063d16171a0e4ef082
mahmoud fouad <mfapolice@gmail.com>
2022-07-14 04:53:29 +0200

    update

--- -- 5. real-time-character-counter/script.js
+++ -- 5. real-time-character-counter/script.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('hi there');
+
+// DOM Variables
+
+const text = document.querySelector('.text');
+const total = document.querySelector('.total');
+const remaining = document.querySelector('.remaining');
+
+// function
+text.addEventListener('input', e => {
+  let totl = 50;
+
+  console.log(e.target.value);
+});
