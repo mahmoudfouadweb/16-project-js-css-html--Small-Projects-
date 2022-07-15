e4201262b1b47bc2b03c213b3850a734e2524301
mahmoud fouad <mfapolice@gmail.com>
2022-07-16 00:38:39 +0200

    update

--- -- 9. video-trailer-popup/script.js
+++ -- 9. video-trailer-popup/script.js
@@ -1,2 +1,5 @@
 'use strict';
-console.log('hi there');
+
+const btn = document.querySelector('.btn');
+const close = document.querySelector('.icon');
+const trailerContainer = document.querySelector('.trailer-container');
