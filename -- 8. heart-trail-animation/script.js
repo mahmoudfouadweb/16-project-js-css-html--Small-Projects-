ff87bbd06236bc93ec3db665f6822a38e0a5215b
mahmoud fouad <mfapolice@gmail.com>
2022-07-15 10:18:13 +0200

    done

--- -- 8. heart-trail-animation/script.js
+++ -- 8. heart-trail-animation/script.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log('hi there');
+
+const body = document.querySelector('body');
+
+body.addEventListener('mousemove', e => {
+  const random = Math.random() * 100;
+  const x = e.offsetX;
+  const y = e.offsetY;
+  const span = document.createElement('span');
+  span.style.left = x + 'px';
+  span.style.top = y + 'px';
+  span.style.width = random + 'px';
+  span.style.height = random + 'px';
+  body.appendChild(span);
+  setTimeout(() => {
+    span.remove();
+  }, 900);
+});
