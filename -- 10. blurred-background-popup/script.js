67729fd6d18c4ab5799fb0f11ca39da8c3261ff2
mahmoud fouad <mfapolice@gmail.com>
2022-07-16 23:07:41 +0200

    update

--- -- 10. blurred-background-popup/script.js
+++ -- 10. blurred-background-popup/script.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log('hi there');
+
+// DOM variables
+const container = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+const icon = document.querySelector('.icon');
+const modal = document.querySelector('.pop-up--modal');
+const modalBtn = document.querySelector('.modal-btn');
+const inputEmail = document.querySelector('.input');
+
+icon.addEventListener('click', () => {
+  modal.style.opacity = 0;
+  container.classList.remove('active');
+});
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+});
