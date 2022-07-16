1a93cca642cff264c1fa3597763ad73c682f451b
mahmoud fouad <mfapolice@gmail.com>
2022-07-16 23:12:56 +0200

    done

--- -- 10. blurred-background-popup/script.js
+++ -- 10. blurred-background-popup/script.js
@@ -11,8 +11,12 @@ const inputEmail = document.querySelector('.input');
 icon.addEventListener('click', () => {
   modal.style.opacity = 0;
   container.classList.remove('active');
+  modal.classList.remove('active');
 });
 
 btn.addEventListener('click', e => {
   e.preventDefault();
+  modal.style.opacity = 1;
+  modal.classList.add('active');
+  container.classList.add('active');
 });
