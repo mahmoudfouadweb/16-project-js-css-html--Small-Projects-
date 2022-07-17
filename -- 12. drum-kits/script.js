cbd506750de97aa225e2d8fccb9590f2cb5a6670
mahmoud fouad <mfapolice@gmail.com>
2022-07-17 22:42:13 +0200

    update

--- -- 12. drum-kits/script.js
+++ -- 12. drum-kits/script.js
@@ -1,2 +1,40 @@
 'use strict';
-console.log('hi there');
+
+// DOM variables
+
+const cards = document.querySelector('.drum-box');
+const crash = document.querySelector('.crash');
+const kick = document.querySelector('.kick');
+const snare = document.querySelector('.snare');
+const tom = document.querySelector('.tom');
+
+// function
+
+const container = ['crash', 'kick', 'snare', 'tom'];
+
+container.forEach(card => {
+  const newDiv = document.createElement('div');
+  const newAudio = document.createElement('audio');
+  newAudio.src = 'sounds/' + card + '.mp3';
+  newDiv.appendChild(newAudio);
+  newDiv.classList.add('drum-card', card);
+  newDiv.textContent = card;
+  cards.appendChild(newDiv);
+  newDiv.addEventListener('click', () => {
+    newAudio.play();
+  });
+});
+
+// crash
+// kick
+// snare
+// tom
+
+container.forEach(card => {
+  window.addEventListener('keydown', e => {
+    console.log(e.key);
+    if (e.key == card.slice(0, 1)) {
+      newAudio.play();
+    }
+  });
+});
