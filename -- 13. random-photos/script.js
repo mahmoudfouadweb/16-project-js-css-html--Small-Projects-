ea782a22c8ca26b731b866b1348bb40ed1ab97a2
mahmoud fouad <mfapolice@gmail.com>
2022-07-17 23:37:43 +0200

    done

--- -- 13. random-photos/script.js
+++ -- 13. random-photos/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.image-container');
+
+btn.addEventListener('click', e => {
+  loadImages();
+});
+
+function loadImages() {
+  for (let i = 0; i < 100; i++) {
+    const random = Math.floor(Math.random() * 100);
+    const img = document.createElement('img');
+    img.src = `https://picsum.photos/300?random=${random}`;
+    container.insertAdjacentElement('beforeend', img);
+  }
+}
