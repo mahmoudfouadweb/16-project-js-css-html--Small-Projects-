1f3cb405decd52899430c328323245f08eb7956f
mahmoud fouad <mfapolice@gmail.com>
2022-07-17 14:47:07 +0200

    first commit

--- -- 10. blurred-background-popup/script.js
+++ -- 10. blurred-background-popup/script.js
@@ -12,10 +12,12 @@ icon.addEventListener('click', () => {
   modal.style.opacity = 0;
   container.classList.remove('active');
   modal.classList.remove('active');
+  modal.style.top = '10%';
 });
 
 btn.addEventListener('click', e => {
   e.preventDefault();
+  modal.style.top = '50%';
   modal.style.opacity = 1;
   modal.classList.add('active');
   container.classList.add('active');
