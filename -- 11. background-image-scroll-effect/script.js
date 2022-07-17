6425c595dc1a78a5ad9bdeeb5c8179efc405865b
mahmoud fouad <mfapolice@gmail.com>
2022-07-17 18:21:03 +0200

    first commit drum-kits

--- -- 11. background-image-scroll-effect/script.js
+++ -- 11. background-image-scroll-effect/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const img = document.querySelector('.bg-image');
+
+window.addEventListener('scroll', () => {
+  updateUi();
+});
+
+function updateUi() {
+  let pos = scrollY / 928;
+  img.style.opacity = 1 - pos;
+  img.style.backgroundSize = 160 - window.scrollY / 14 + '%';
+  console.log(scrollY);
+}
