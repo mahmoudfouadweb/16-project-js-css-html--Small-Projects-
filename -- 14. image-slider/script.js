abd8ec022f3b494a5f0a4c7df378dd99eb988023
mahmoud fouad <mfapolice@gmail.com>
2022-07-19 14:55:59 +0200

    done

--- -- 14. image-slider/script.js
+++ -- 14. image-slider/script.js
@@ -1,7 +1,37 @@
 'use strict';
 
 // DOM variables
-const container = document.querySelector('.container');
-const imgs = document.querySelector('.img');
-const right = document.querySelector('.right');
-const left = document.querySelector('.left');
+const container = document.querySelector('.image-container');
+const imgs = document.querySelectorAll('.img');
+const right = document.querySelector('.next');
+const left = document.querySelector('.prev');
+let current = 1;
+let timeout;
+const imgLenght = imgs.length;
+///////////////////////////////////////////////////////
+
+right.addEventListener('click', () => {
+  clearTimeout(timeout);
+  current++;
+  updateUI();
+});
+
+left.addEventListener('click', () => {
+  clearTimeout(timeout);
+  current--;
+  updateUI();
+});
+
+function updateUI() {
+  if (current > imgLenght) {
+    current = 1;
+  } else if (current < 1) {
+    current = imgLenght;
+  }
+  container.style.transform = `translateX(-${(current - 1) * 500}px)`;
+  timeout = setTimeout(() => {
+    current++;
+    updateUI();
+  }, 3000);
+}
+updateUI();
