0890c94a3fd00f6ed1a3f74814b1e7c2fd2c11bd
mahmoud fouad <mfapolice@gmail.com>
2022-07-19 12:46:09 +0200

    dom

--- -- 14. image-slider/script.js
+++ -- 14. image-slider/script.js
@@ -1,3 +1,7 @@
 'use strict';
 
 // DOM variables
+const container = document.querySelector('.container');
+const imgs = document.querySelector('.img');
+const right = document.querySelector('.right');
+const left = document.querySelector('.left');
